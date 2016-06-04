18cd286d41d7ad450a71fa0dc8dc7d9640acda2a
paulwasit <plat.sebastien@hotmail.fr>
2016-06-05 01:24:00 +0200

    update server port mgmt

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index.html');
 }); 
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
